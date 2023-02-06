518c49c2b652ad3b642204846216777f6fbe6a1c
eu-bin.kim <eu-bin.kim@tripstore.kr>
2023-02-06 17:23:15 +0900

    removed a duplicate test

--- week1/tests/test_6.py
+++ week1/tests/test_6.py
@@ -58,17 +58,6 @@ def test_gpt_v3_logits_order_is_preserved():
     assert not torch.allclose(logits[:, 2, :], logits[:, 3, :])
 
 
-# --- compare gpt_v2 and gpt_v3 --- #
-def test_gpt_v2_and_head_v4_generates_text_given_a_context():
-    torch.manual_seed(1337)
-    head = HeadVer4(config['block_size'], config['embed_size'])
-    lm = GPTVer2(head, config['vocab_size'], config['embed_size'], config['block_size'])
-    train(lm)  # may take a while
-    expected = "The quick brown fox jumps over the lazyor th manot utou s l spaif ant"
-    was = generate(lm, "The quick brown fox jumps over the lazy", 30)
-    assert expected == was
-
-
 def test_gpt_v3_and_head_v4_generates_text_given_a_context():
     torch.manual_seed(1337)
     head = HeadVer4(config['block_size'], config['embed_size'])
