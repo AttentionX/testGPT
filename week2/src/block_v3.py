16d5b4635b05bdc3d33786f5c8f8517bfa3983a6
eu-bin.kim <eu-bin.kim@tripstore.kr>
2023-02-13 21:21:14 +0900

    test for dropout

--- week2/src/block_v3.py
+++ week2/src/block_v3.py
@@ -3,13 +3,13 @@ adding LayerNorm to the block
 """
 import torch
 from typing import Union
-from .block_v1 import BlockVer1
 from .multi_head_v1 import MultiHeadVer1
 from .multi_head_v2 import MultiHeadVer2
+from .block_v2 import BlockVer2
 from .block_v3_ln import LayerNorm
 
 
-class BlockVer3(BlockVer1):
+class BlockVer3(BlockVer2):
 
     def __init__(self, head: Union[MultiHeadVer1, MultiHeadVer2], embed_size: int):
         super().__init__(head, embed_size)
--- week2/tests/test_10.py
+++ week2/tests/test_10.py
@@ -1,3 +1,42 @@
 """
 running question: why do we need Dropout?
 """
+import torch
+from ..src.block_v4 import BlockVer4
+from ..src.multi_head_v1 import MultiHeadVer1
+
+
+def test_block_ver_4_output_is_always_different_in_train_mode():
+    B, T, C = 32, 64, 512
+    n_heads = 8
+    dropout = 0.1
+    block = BlockVer4(MultiHeadVer1(T, C, n_heads), C, dropout)
+    block.train()
+    x = torch.randn(B, T, C)
+    out_1 = block(x)
+    out_2 = block(x)
+    out_3 = block(x)
+    out_4 = block(x)
+    assert not torch.allclose(out_1, out_2)
+    assert not torch.allclose(out_2, out_3)
+    assert not torch.allclose(out_3, out_4)
+
+
+def test_block_ver_4_output_is_always_the_same_in_eval_mode():
+    B, T, C = 32, 64, 512
+    n_heads = 8
+    dropout = 0.1
+    block = BlockVer4(MultiHeadVer1(T, C, n_heads), C, dropout)
+    block.eval()
+    x = torch.randn(B, T, C)
+    out_1 = block(x)
+    out_2 = block(x)
+    out_3 = block(x)
+    out_4 = block(x)
+    assert torch.allclose(out_1, out_2)
+    assert torch.allclose(out_2, out_3)
+    assert torch.allclose(out_3, out_4)
+
+
+def test_dropout_mitigates_overfitting():
+    pass
