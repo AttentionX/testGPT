23bb657899c25a3d376ffb930c714a6bcae4603c
eu-bin.kim <eu-bin.kim@tripstore.kr>
2023-02-14 22:19:59 +0900

    이제 마지막 테스트만

--- week2/src/multi_head_v2.py
+++ week2/src/multi_head_v2.py
@@ -6,7 +6,7 @@ class MultiHeadVer2(torch.nn.Module):
     multi-head self attention (fast)
     """
 
-    def __init__(self, embed_size: int, n_heads: int):
+    def __init__(self, block_size: int, embed_size: int, n_heads: int):
         """
         :param block_size: 32 (문장 속 토큰의 개수)
         :param embed_size: 512 ( 히든 벡처 차원의 크기)
@@ -16,10 +16,13 @@ class MultiHeadVer2(torch.nn.Module):
         """
         super().__init__()
         assert embed_size % n_heads == 0
+        self.n_heads = n_heads
         self.head_size = embed_size // n_heads
         self.query = torch.nn.Linear(embed_size, embed_size, bias=False)
         self.key = torch.nn.Linear(embed_size, embed_size, bias=False)
         self.value = torch.nn.Linear(embed_size, embed_size, bias=False)
+        self.proj = torch.nn.Linear(embed_size, embed_size)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -28,19 +31,32 @@ class MultiHeadVer2(torch.nn.Module):
         """
         # --- TODO --- #
         B, T, C = x.shape
-
-        q = self.query(x)  # (N, L, H) * (H, H) -> (N, L, H)
-        k = self.key(x)  # (N, L, H) * (H, H) -> (N, L, H)
-        v = self.linear(x)  # (N, L, H) * (H, H) -> (N, L, H)
+        q = self.query(x)  # (B, T, C) @ (C, C) -> (B, T, C)
+        k = self.key(x)  # (B, T, C) @ (C, C) -> (B, T, C)
+        v = self.value(x)  # (B, T, C) @ (C, C) -> (B, T, C)
         # split q, k, v into multi-heads
-        q = q.view(B, self.max_length, self.heads, self.head_size)  # (N, L, H) -> (N, L, heads, head_size)
-        k = k.view(B, self.max_length, self.heads, self.head_size)  # (N, L, H) -> (N, L, heads, head_size)
-        v = v.view(B, self.max_length, self.heads, self.head_size)  # (N, L, H) -> (N, L, heads, head_size)
+        q = q.contiguous().view(B, T, self.n_heads, self.head_size)  # (B, T, C) -> (B, T, n_heads, head_size)
+        k = k.contiguous().view(B, T, self.n_heads, self.head_size)  # (B, T, C) -> (B, T, n_heads, head_size)
+        v = v.contiguous().view(B, T, self.n_heads, self.head_size)  # (B, T, C) -> (B, T, n_heads, head_size)
         # make q, k and v matmul-compatible
-        q = q.transpose(1, 2)  # (N, L, heads, head_size) -> (N, heads, L, head_size)
-        k = k.transpose(1, 2)  # (N, L, heads, head_size) -> (N, heads, L, head_size)
-        v = v.transpose(1, 2)  # (N, L, heads, head_size) -> (N, heads, L, head_size)
-
-        out  = ...
+        q = q.transpose(1, 2)  # (B, T, n_heads, head_size) -> (B, n_heads, T, head_size)
+        k = k.transpose(1, 2)  # (B, T, n_heads, head_size) -> (B, n_heads, T, head_size)
+        v = v.transpose(1, 2)  # (B, T, n_heads, head_size) -> (B, n_heads, T, head_size)
+        # compute attention scores
+        # Q * K^T:  compute query-key similarities
+        # (B, n_heads, T, head_size), (B, n_heads, T, head_size) ->  (B, n_heads, T, T)
+        wei = torch.einsum("...qh,...kh->...qk", q, k)
+        # Q * K^T / sqrt(d_k): down-scale similarities to prevent gradient vanishing
+        wei = wei * C ** -0.5
+        # apply padding mask and/or subsequent mask
+        wei = wei.masked_fill(self.tril[:T, :T], float("-inf"))
+        # softmax(Q * K^T / sqrt(d_k)): normalise the sims over keys
+        wei = torch.softmax(wei, dim=-1)
+        # softmax(Q * K^T / sqrt(d_k)) * V: soft-align values with respect to each query
+        # (B, n_heads, T, T),  (B, n_heads, T, head_size) -> (B, n_heads, T, head_size)
+        wei = torch.einsum("...qv,...vh->...qh", wei, v)
+        wei = wei.transpose(1, 2)  # (B, n_heads, T, head_size) -> (B, T, n_heads, head_size)
+        wei = wei.contiguous().view(B, T, C)  # (B, T, n_heads, head_size) -> (B, T, C)
+        out = self.proj(wei)  # (B, T, C) @ (C, C) -> (B, T, C)
         # ------------ #
         return out
--- week2/tests/test_7.py
+++ week2/tests/test_7.py
@@ -1,20 +1,48 @@
 """
 Running question: why do we need more than one head?
 """
+import timeit
+
 import torch
 from ..src.multi_head_v1_head_v4 import HeadVer4
 from ..src.multi_head_v1 import MultiHeadVer1
+from ..src.multi_head_v2 import MultiHeadVer2
 import random
 
 
 # test: multi-head ver 2 is logically the same as multi-head ver 1
 def test_multi_head_ver_2_is_logically_the_same_as_multi_head_ver_1():
-    pass
+    B, T, C = 32, 64, 512
+    n_heads = 8
+    multi_head_v1 = MultiHeadVer1(T, C, n_heads)
+    multi_head_v2 = MultiHeadVer2(T, C, n_heads)
+    # --- head v1 --- #
+    for head in multi_head_v1.heads:
+        # initialize weights with all zeros
+        head.query.weight = torch.nn.Parameter(torch.zeros_like(head.query.weight))
+        head.key.weight = torch.nn.Parameter(torch.zeros_like(head.key.weight))
+        head.value.weight = torch.nn.Parameter(torch.zeros_like(head.value.weight))
+    # --- head v2 --- #
+    multi_head_v2.query.weight = torch.nn.Parameter(torch.zeros_like(multi_head_v2.query.weight))
+    multi_head_v2.key.weight = torch.nn.Parameter(torch.zeros_like(multi_head_v2.key.weight))
+    multi_head_v2.value.weight = torch.nn.Parameter(torch.zeros_like(multi_head_v2.value.weight))
+    multi_head_v2.proj = multi_head_v1.proj
+    x = torch.randn(B, T, C)
+    out_v1 = multi_head_v1(x)
+    out_v2 = multi_head_v2(x)
+    assert torch.allclose(out_v1, out_v2)
 
 
 # test: multi-head ver 2 is faster than ver 1
 def test_multi_head_ver_2_is_faster_than_ver_1():
-    pass
+    B, T, C = 32, 64, 512
+    n_heads = 8
+    x = torch.randn(B, T, C)
+    multi_head_v1 = MultiHeadVer1(T, C, n_heads)
+    multi_head_v2 = MultiHeadVer2(T, C, n_heads)
+    time_taken_v1 = timeit.timeit(lambda: multi_head_v1(x), number=10)
+    time_taken_v2 = timeit.timeit(lambda: multi_head_v2(x), number=10)
+    assert time_taken_v2 < time_taken_v1
 
 
 # test: gpt learns faster with multi-head
